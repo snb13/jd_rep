683d0f45c85b9862eb29b0da1c0f1fc21993ca07
snb13 <77163124+snb13@users.noreply.github.com>
2023-01-21 11:52:43 +0300

    AnotherClass.java created and hi instance added

---
+++ src/main/java/AnotherClass.java
@@ -0,0 +1,11 @@
+public class AnotherClass {
+    
+
+    public AnotherClass(){
+    }
+
+    public  void PrintHello(String hello) {
+        System.out.println(hello + " from another class");
+    }
+
+}
